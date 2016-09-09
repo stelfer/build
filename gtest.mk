0bf38ea2a324b54962d3b495d031901863615dc2
Soren Telfer <soren.telfer@gmail.com>
2016-09-09 14:08:26 -0700

    Updated googletest to v1.8
    - 1.8 now hosted on github, also some directory changes etc

--- gtest.mk
+++ gtest.mk
@@ -1,21 +1,21 @@
 #Copyright (C) 2015 by AT&T Services Inc. MIT License. See LICENSE.txt
 
-GTEST_VER		:= 1.7.0
+GTEST_VER		:= 1.8.0
 GTEST			:= gtest-$(GTEST_VER)
 GTEST_UNPACK		:= $(OPTDIR)/$(GTEST)/.unpack
 GTEST_INSTALL		:= $(OPTDIR)/$(GTEST)/.install
-$(GTEST)_ARCHIVE 	:= $(GTEST).zip
-$($(GTEST)_ARCHIVE)_URL := https://googletest.googlecode.com/files/$($(GTEST)_ARCHIVE)
+$(GTEST)_ARCHIVE 	:= release-$(GTEST_VER).tar.gz
+$($(GTEST)_ARCHIVE)_URL := https://codeload.github.com/google/googletest/tar.gz/release-$(GTEST_VER)
 $(GTEST_UNPACK) 	: $(DOWNLOADS)/$($(GTEST)_ARCHIVE)
 
 include $(GTEST_INSTALL)
 
 $(GTEST_INSTALL): | $(OPTDIR)/$(LLVM)/.build/.install $(GTEST_UNPACK)
 	mkdir -p build/include $(LIBDIR)
-	cp -af $(OPTDIR)/$(GTEST)/include/gtest $(INCLUDEDIR)
-	$(CXX) $(CXXSTD) $(CXXINCLUDES) -I$(OPTDIR)/$(GTEST) -c -o $(LIBDIR)/libgtest.o $(OPTDIR)/$(GTEST)/src/gtest-all.cc
+	cp -af $(OPTDIR)/$(GTEST)/googletest/include/gtest $(INCLUDEDIR)
+	$(CXX) $(CXXSTD) $(CXXINCLUDES) -I$(OPTDIR)/$(GTEST)/googletest -c -o $(LIBDIR)/libgtest.o $(OPTDIR)/$(GTEST)/googletest/src/gtest-all.cc
 	ar -rv $(LIBDIR)/libgtest.a $(LIBDIR)/libgtest.o
-	$(CXX) $(CXXSTD) $(CXXINCLUDES) -I$(OPTDIR)/$(GTEST) -emit-llvm -c -o $(LIBDIR)/gtest.bc $(OPTDIR)/$(GTEST)/src/gtest-all.cc
+	$(LLVM_BC_COMPILE_CXX) -I$(OPTDIR)/$(GTEST)/googletest -c -o $(LIBDIR)/gtest.bc $(OPTDIR)/$(GTEST)/googletest/src/gtest-all.cc
 	touch $@
 
 
