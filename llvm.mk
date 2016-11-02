92ecd8f872205287ee2c129479a459c95a06d946
Soren Telfer <soren.telfer@gmail.com>
2016-11-01 17:20:44 -0700

    Fixed some stray text

--- llvm.mk
+++ llvm.mk
@@ -172,7 +172,7 @@ $(BUILD)/test/%.ll : $(BUILD)/test/%.bc
 
 $(BUILD)/test/%.pass.xml : $(BUILD)/test/%.ll
 	p=( $(TARGET_HOSTS) );	n=$$(( RANDOM % $${#p[@]} )); h=$${p[$$n]};\
-	echo "[==========]" Running on $$h $(TARGET_OBJS);\
+	echo "[==========]" Running on $$h;\
 	rsync $< $(TARGET_OBJS) $(BUILD)/target.sh $$h:build-$(TARGET_OS) ;\
 	$(MAKE) $(BUILD)/target-$(TARGET_MODE)/$< HOST=$$h TARGET_OBJS="$(TARGET_OBJS)"
 	python $(BUILD)/parse_perf_tests.py $(@D)/$(*F).xml $@
